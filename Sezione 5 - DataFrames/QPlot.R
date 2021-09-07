5b30d7ef302ee9211466588af1d4badc5c78161e
Matteo <cinethick@gmail.com>
2021-09-07 22:33:22 +0200

    conclusione delle lezioni sezione 5

--- Sezione 5 - DataFrames/QPlot.R
+++ Sezione 5 - DataFrames/QPlot.R
@@ -11,5 +11,42 @@ qplot(data=statistiche, x=Income.Group, y=Birth.rate, size=I(10)) # Per modifica
 qplot(data=statistiche, x=Income.Group, y=Birth.rate, size=I(3), color=I("blue")) # colore
 qplot(data=statistiche, x=Income.Group, y=Birth.rate, geom="boxplot") # tipologia
 
+?I
+# Sta per INIBIRE (la conversione o interpretazione degli oggetti)
+
 qplot(data=statistiche, x=Internet.users, y=Birth.rate,
       size=I(4), color=Income.Group)
+
+# importare i vettori da "CountryRegionVectors.R"
+
+# Creare il DataFrame
+
+datiPerRegione <- data.frame(Countries_2012_Dataset, Codes_2012_Dataset, Regions_2012_Dataset)
+# esplorazione
+head(datiPerRegione)
+levels(factor(datiPerRegione$Regions_2012_Dataset))
+
+#pulizia
+colnames(datiPerRegione) <- c("Nazione", "Codice", "Regione")
+head(datiPerRegione)
+
+# al posto che riordinare il DF si predispone dall'inizio:
+rm(datiPerRegione)
+datiPerRegione <- data.frame(Nazione=Countries_2012_Dataset, Codice=Codes_2012_Dataset,
+                             Regione=Regions_2012_Dataset)
+head(datiPerRegione)
+tail(datiPerRegione)
+
+summary(datiPerRegione)
+
+# Merging / Unione dei Dataframe
+datiUniti <- merge(statistiche, datiPerRegione, by.x = "Country.Code", by.y = "Codice")
+head(datiUniti)
+
+datiUniti$Nazione <- NULL
+str(datiUniti)
+
+qplot(data=datiUniti, x=Internet.users, y=Birth.rate,
+      size=I(10), color=Regione, shape=I(17), alpha=I(0.5),
+      main="Nascite su Utenti Internet")
+
